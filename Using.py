d671bfa625d36fca9e5b255d57c676e2c9cde5aa
HuangfuBing <90957752+HuangfuBing@users.noreply.github.com>
2022-08-22 15:35:54 +0800

    2.0
    加入了使用用例

---
+++ Using.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import cv2 as cv
+
+def image_process(image_path):
+    tf.compat.v1.disable_eager_execution()
+    image=cv.imread(image_path)
+    image=cv.resize(image,(224,224))
+    image=tf.convert_to_tensor(image)
+    image=tf.reshape(image,[1,224,224,3])
+    image = tf.cast(image, dtype=np.float32)
+    sess = tf.compat.v1.Session()
+    sess.run(tf.compat.v1.global_variables_initializer())
+    image = image.eval(session=sess)  # 转化为numpy数组
+    return image
+
+image_path="F:/test/2.jpg"
+image=image_process(image_path)
+print(image)
+interpreter = tf.lite.Interpreter("./model.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+print(input_details)
+print(output_details)
+interpreter.set_tensor(input_details[0]['index'], image)#传入的数据必须为ndarray类型
+interpreter.invoke()
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
